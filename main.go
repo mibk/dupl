cbd9d685e5fa6e8f1d8bf9191507a245552977d3
Michal Bohuslávek <mbohuslavek@gmail.com>
2017-11-13 12:37:36 +0100

    Move usage func to bottom

--- main.go
+++ main.go
@@ -20,60 +20,23 @@ const defaultThreshold = 15
 
 var (
 	paths     = []string{"."}
-	vendor    = flag.Bool("vendor", false, "check files in vendor directory")
-	verbose   = flag.Bool("verbose", false, "explain what is being done")
-	threshold = flag.Int("threshold", defaultThreshold, "minimum token sequence as a clone")
-	files     = flag.Bool("files", false, "files names from stdin")
+	vendor    = flag.Bool("vendor", false, "")
+	verbose   = flag.Bool("verbose", false, "")
+	threshold = flag.Int("threshold", defaultThreshold, "")
+	files     = flag.Bool("files", false, "")
 
-	html     = flag.Bool("html", false, "html output")
-	plumbing = flag.Bool("plumbing", false, "plumbing output for consumption by scripts or tools")
+	html     = flag.Bool("html", false, "")
+	plumbing = flag.Bool("plumbing", false, "")
 )
 
 const (
 	vendorDirPrefix = "vendor" + string(filepath.Separator)
-	vendorDirInPath = string(filepath.Separator) + "vendor" + string(filepath.Separator)
+	vendorDirInPath = string(filepath.Separator) + vendorDirPrefix
 )
 
 func init() {
 	flag.BoolVar(verbose, "v", false, "alias for -verbose")
 	flag.IntVar(threshold, "t", defaultThreshold, "alias for -threshold")
-}
-
-func usage() {
-	fmt.Fprintln(os.Stderr, `Usage of dupl:
-  dupl [flags] [paths]
-
-Paths:
-  If the given path is a file, dupl will use it regardless of
-  the file extension. If it is a directory it will recursively
-  search for *.go files in that directory.
-
-  If no path is given dupl will recursively search for *.go
-  files in the current directory.
-
-Flags:
-  -files
-    	read file names from stdin one at each line
-  -html
-    	output the results as HTML, including duplicate code fragments
-  -plumbing
-    	plumbing (easy-to-parse) output for consumption by scripts or tools
-  -t, -threshold size
-    	minimum token sequence size as a clone (default 15)
-  -vendor
-    	check files in vendor directory
-  -v, -verbose
-    	explain what is being done
-
-Examples:
-  dupl -t 100
-    	Search clones in the current directory of size at least
-    	100 tokens.
-  dupl $(find app/ -name '*_test.go')
-    	Search for clones in tests in the app directory.
-  find app/ -name '*_test.go' |dupl -files
-    	The same as above.`)
-	os.Exit(2)
 }
 
 func main() {
@@ -216,4 +179,40 @@ func unique(group [][]*syntax.Node) [][]*syntax.Node {
 		}
 	}
 	return newGroup
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, `Usage: dupl [flags] [paths]
+
+Paths:
+  If the given path is a file, dupl will use it regardless of
+  the file extension. If it is a directory, it will recursively
+  search for *.go files in that directory.
+
+  If no path is given, dupl will recursively search for *.go
+  files in the current directory.
+
+Flags:
+  -files
+    	read file names from stdin one at each line
+  -html
+    	output the results as HTML, including duplicate code fragments
+  -plumbing
+    	plumbing (easy-to-parse) output for consumption by scripts or tools
+  -t, -threshold size
+    	minimum token sequence size as a clone (default 15)
+  -vendor
+    	check files in vendor directory
+  -v, -verbose
+    	explain what is being done
+
+Examples:
+  dupl -t 100
+    	Search clones in the current directory of size at least
+    	100 tokens.
+  dupl $(find app/ -name '*_test.go')
+    	Search for clones in tests in the app directory.
+  find app/ -name '*_test.go' |dupl -files
+    	The same as above.`)
+	os.Exit(2)
 }
